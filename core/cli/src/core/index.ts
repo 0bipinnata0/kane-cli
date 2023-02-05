f0bd5280d8720d6f0258cb0b69b1683d8fa0714d
tom <odinzero@outlook.com>
2023-02-05 21:03:34 +0800

    判断本地环境

---
+++ core/cli/src/core/index.ts
@@ -0,0 +1,5 @@
+function core(...args: string[]) {
+	console.info('exec core', args);
+}
+
+export default core;
--- core/cli/src/index.ts
+++ core/cli/src/index.ts
@@ -1,3 +1,10 @@
-const sum = (a: number, b: number) => a + b;
-export default sum;
-console.info('abc', sum(12, 2));
+import importLocal = require('import-local');
+import * as npmlog from 'npmlog';
+import core from './core';
+
+console.info('__filename', __filename);
+if (importLocal(__filename)) {
+	npmlog.info('cli', '正在使用 kane-cli 本地版本');
+} else {
+	core(...process.argv.slice(2));
+}
