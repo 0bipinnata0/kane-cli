5da468dfda6727438be3997e9d6a5b97a9c802c9
tom <odinzero@outlook.com>
2023-02-05 22:12:06 +0800

    如果要读取package.json，需要修改配置

--- core/cli/src/core/index.ts
+++ core/cli/src/core/index.ts
@@ -1,5 +1,12 @@
-function core(...args: string[]) {
-	console.info('exec core', args);
+import pkg from '../../package.json';
+
+function core(...___: string[]) {
+	console.info('>exec core');
+	checkPkgVersion();
+}
+
+function checkPkgVersion() {
+	console.info(pkg.version);
 }
 
 export default core;
--- core/cli/src/index.ts
+++ core/cli/src/index.ts
@@ -1,8 +1,8 @@
-import importLocal = require('import-local');
+import importLocal from 'import-local';
 import * as npmlog from 'npmlog';
 import core from './core';
 
-console.info('__filename', __filename);
+console.info('__file', __filename);
 if (importLocal(__filename)) {
 	npmlog.info('cli', '正在使用 kane-cli 本地版本');
 } else {
